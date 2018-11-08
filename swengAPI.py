2469e5026058edb6823c9aa5a44a728e0f84e56c
nualand <nidhomnu@tcd.ie>
2018-11-08 15:06:20 +0000

    working now

--- swengAPI.py
+++ swengAPI.py
@@ -6,15 +6,21 @@ from github import Github
 # First create a Github instance:
 
 # using username and password
-g = Github("user", "password")
+#g = Github("user", "password")
 
 # or using an access token
 access_token = "17998201548700064e81c33c95b73e3329973202"
-g = Github(access_token)
+myGithub = Github(access_token)
 
-# Github Enterprise with custom hostname
-g = Github(base_url="https://{hostname}/api/v3", login_or_token=access_token)
-
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
+# prints all my repos
+for repo in myGithub.get_user().get_repos():
     print(repo.name)
+
+
+# prints the contests of a repo
+this_repo = "nualand/SoftwareE"
+print("The content of " + this_repo)
+repo = myGithub.get_repo(this_repo)
+contents = repo.get_contents("")
+for content_file in contents:
+    print(content_file)
